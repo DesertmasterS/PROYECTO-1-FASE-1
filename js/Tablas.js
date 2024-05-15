21932dca338505b49f20965d6635b061ec54c518
vent30491 <vargasbenjamin847@gmail.com>
2024-05-14 23:46:15 -0400

    carrusel

--- js/Tablas.js
+++ js/Tablas.js
@@ -1,10 +1,81 @@
 import platos from './json/platos.json' assert {type: 'json'};
-console.log(platos);
 
-$(document).ready(function () {
-    $.get(platos,function (data) {
-            $.each(data.categories, function (i, tmp) {
-                console.log("holi");
-            })
+console.log(platos);
+/*
+let cont = document.createElement('div');
+cont.classList.add('owl-carousel', 'owl-theme','food-container');
+
+
+platos.forEach(plato => {
+    
+    let cardsHTML = `
+        <div class="item">
+            <a href="/html/DetailProduct.html">
+                <div class="card comida" style="width: 16rem; height: 26rem;">
+                    <div class="img">
+                        <img src="${plato.imagen}" class="card-img-top" alt="..." >
+                    </div>
+                    <div class="card-body">
+                    <h5 class="card-title">${plato.nombre}</h5>
+                    <hr>
+                    <p class="card-text">${plato.descripcion}</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    `;    
+    cont += cardsHTML;
+    let contenedor = document.getElementById('Carrusel');
+        contenedor.appendChild(cont);
+});
+*/
+let carouselContainer = document.createElement('div');
+        carouselContainer.classList.add('owl-carousel', 'owl-theme', 'food-container');
+
+        platos.forEach(plato => {
+            let itemHTML = `
+                <div class="item">
+                    <a href="/html/DetailProduct.html">
+                        <div class="card comida" style="width: 16rem; height: 26rem;">
+                            <div class="img">
+                                <img src="${plato.imagen}" class="card-img-top" alt="${plato.nombre}" onerror="this.src='/img/platos/default.jpg';">
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">${plato.nombre}</h5>
+                                <hr>
+                                <p class="card-text">${plato.descripcion}</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            `;
+            carouselContainer.innerHTML += itemHTML;
+        });
+
+        let contenedor = document.getElementById('Carrusel');
+        contenedor.appendChild(carouselContainer);
+
+        $('.owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:2
+                },
+                1000:{
+                    items:3
+                }
+            }
         })
-})
+        var owl = $('.owl-carousel');
+        owl.owlCarousel();
+        $('.r').click(function() {
+            owl.trigger('next.owl.carousel');
+        })
+        $('.l').click(function() {
+            owl.trigger('prev.owl.carousel', [300]);
+        })
